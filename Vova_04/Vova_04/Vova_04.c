d1c97c757999a84c9da70a8cac7a14154d8afbbf
Максим Федотов <fedotom2@gmail.com>
2020-09-27 00:50:50 +0300

    Lab04

---
+++ Vova_04/Vova_04/Vova_04.c
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#define MAX_SIZE 256
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char str1[MAX_SIZE];
+	int len;
+
+	printf("Введите строку\n");
+	fgets(str1, MAX_SIZE, stdin);
+	fflush(stdin);
+
+	len = strlen(str1);
+
+	int j = 0;
+	for (int i = len - 1; i >= 0; i--) {
+		if (str1[i] == ' ') {
+			j = i;
+			break;
+		}
+	}
+
+	printf("\n");
+	printf("Последние слово в предложении\n");
+	printf(&str1[j+1]);
+
+	return 0;
+}
---
+++ Vova_04/Vova_04_2/Vova_04_2.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#define MAX_SIZE 256
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	char str1[MAX_SIZE];
+	char str2[MAX_SIZE];
+	int len;
+
+	printf("Введите строку\n");
+	fgets(str1, MAX_SIZE, stdin);
+	fflush(stdin);
+
+	len = strlen(str1) / 2;
+
+	for (int i = 0; i < len; i++)
+		str2[i] = str1[i];
+
+	str2[len] = '\0';
+
+	printf("\n");
+	printf("Первая половина строки\n");
+	printf(str2);
+
+	return 0;
+}
