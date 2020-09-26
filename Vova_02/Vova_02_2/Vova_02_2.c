50f0941312745b74849f835d9b58dc2ea2717bf1
Максим Федотов <fedotom2@gmail.com>
2020-09-26 23:16:12 +0300

    Change Lab02

--- Vova_02/Vova_02_2/Vova_02_2.c
+++ Vova_02/Vova_02_2/Vova_02_2.c
@@ -1,9 +1,9 @@
 ﻿#include <stdio.h>
+#define N 10
 
 int main() 
 {
-	const int N = 10;
-	int arr[10] = { 2, 4, 0, 99, 3, 5, 7, -8, 1, 6 };
+	int arr[N] = { 2, 4, 0, 99, 3, 5, 7, -8, 1, 6 };
 
 	int i = 0;
 	int max = arr[i];
